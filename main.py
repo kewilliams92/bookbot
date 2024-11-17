ecf8279952f71394edff842cbe7623d1eaded7e7
kewilliams92 <kewilliamsdev@gmail.com>
2024-11-16 20:03:17 -0500

    placed the count of characters into a dictionary

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ def main():
     book_text = get_book_text(book_path)
     word_count = get_book_count(book_text)
     print(book_text, word_count)
+    character_count = get_character_count(book_text)
+    print(character_count)
 
 
 def get_book_text(book_path):
@@ -13,6 +15,19 @@ def get_book_text(book_path):
 def get_book_count(text):
     words = text.split()
     return len(words)
+
+
+def get_character_count(words):
+    characters = {}
+    lowercase_words = words.lower()
+    split_words = lowercase_words.split()
+    for words in split_words:
+        for char in words:
+            if char in characters:
+                characters[char] += 1
+            else:
+                characters[char] = 1
+    return characters
 
 
 main()

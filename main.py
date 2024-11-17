4092d635ca6795c311ee7bdca611f1fc4bc16487
kewilliams92 <kewilliamsdev@gmail.com>
2024-11-16 19:39:19 -0500

    main.py can read book text

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("hello world")
+def main():
+    book_path = "books/frankenstein.txt"
+    book_text = get_book_text(book_path)
+    print(book_text)
+
+
+def get_book_text(book_path):
+    with open(book_path) as f:
+        file_content = f.read()
+        print(file_content)
+
+
+main()
